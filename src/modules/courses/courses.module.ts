0cea262a76e166120354073840b67ecadcbdaafd
nwosum113-ui <nwosum113@gmail.com>
2026-07-29 16:07:44 +0100

    Add course search autocomplete API

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -4,13 +4,15 @@ import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { RecommendationsController } from './recommendations.controller';
 import { RecommendationsService } from './recommendations.service';
+import { SearchController } from './search.controller';
+import { SearchService } from './search.service';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { FeeCalculatorModule } from '../billing/fee-calculator.module';
 
 @Module({
   imports: [NotificationsModule, FeeCalculatorModule],
-  controllers: [CoursesController, RecommendationsController],
-  providers: [CoursesService, RecommendationsService],
-  exports: [CoursesService, RecommendationsService],
+  controllers: [SearchController, CoursesController, RecommendationsController],
+  providers: [CoursesService, RecommendationsService, SearchService],
+  exports: [CoursesService, RecommendationsService, SearchService],
 })
 export class CoursesModule {}
